d36aefca20b387377b50ebf70e34fa98dd945205
loic <loic.python@gmail.com>
2019-03-06 09:27:37 +0100

    Formatage du tweet OKAYYY

--- app/src/main/java/ch/ribeiropython/twitterproject/Tweet.java
+++ app/src/main/java/ch/ribeiropython/twitterproject/Tweet.java
@@ -2,12 +2,20 @@ package ch.ribeiropython.twitterproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class Tweet extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tweet);
+
+
+
     }
 }
