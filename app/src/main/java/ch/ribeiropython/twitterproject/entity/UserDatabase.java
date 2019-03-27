0d687e93cdab16a14122691b58c0ed3b53ba293a
loic <loic.python@gmail.com>
2019-03-27 14:52:08 +0100

    Db Tweet et user req multi

---
+++ app/src/main/java/ch/ribeiropython/twitterproject/entity/UserDatabase.java
@@ -0,0 +1,42 @@
+package ch.ribeiropython.twitterproject.entity;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {UserEntity.class}, version = 1, exportSchema = false)
+public abstract class UserDatabase extends RoomDatabase {
+
+    private static UserDatabase INSTANCE;
+
+    // For Singleton instantiation
+    private static final Object LOCK = new Object();
+
+    public abstract UserDao UserDao();
+
+    public synchronized static UserDatabase getAppDatabase(Context context) {
+        if (INSTANCE == null) {
+            synchronized (LOCK) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(), UserDatabase.class, "user-database")
+                            /*
+                            allow queries on the main thread.
+                            Don't do this in a real app!
+                            See PersistenceBasicSample
+                            https://github.com/googlesamples/android-architecture-components/tree/master/BasicSample
+                            for an example.
+
+                            Would throw java.lang.IllegalStateException:
+                            Cannot access database on the main thread since it may potentially lock the UI for a long period of time.
+                            */
+                            .allowMainThreadQueries()
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+}
