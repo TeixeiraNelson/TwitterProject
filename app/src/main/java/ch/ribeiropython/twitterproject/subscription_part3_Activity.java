e144da0fc9487311f54e8bb54f9ffe580f75ee9d
Nelson <ribeiroteixeira.nelson@gmail.com>
2019-04-16 14:29:40 +0200

    Firebase

--- app/src/main/java/ch/ribeiropython/twitterproject/subscription_part3_Activity.java
+++ app/src/main/java/ch/ribeiropython/twitterproject/subscription_part3_Activity.java
@@ -1,7 +1,5 @@
 package ch.ribeiropython.twitterproject;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
@@ -9,8 +7,6 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.TextView;
-
-import com.google.gson.Gson;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -31,7 +27,7 @@ public class subscription_part3_Activity extends AppCompatActivity {
         btn.setVisibility(View.VISIBLE);
 
         /*
-        Check box events
+            Check box events
          */
 
 
@@ -57,56 +53,31 @@ public class subscription_part3_Activity extends AppCompatActivity {
 
     public void startApp(View view){
         /*
-        Method that starts the app (menu activity)
+            Method that starts the app (menu activity)
          */
         Bundle extras = getIntent().getExtras();
         User user;
         if (extras != null) {
             user = (User) getIntent().getSerializableExtra("User");
-            saveToDataBase(user);
-            saveUserToSession(user);
+            registerUser(user);
         }
 
 
 
 
 
-        /*
-        Launching app.
-         */
-        Intent intentMyAccount = new Intent(getApplicationContext(), Menu.class);
-        startActivity(intentMyAccount);
-        finish();
+
 
     }
 
-    public void saveToDataBase(User user){
+    public void registerUser(User user){
         /*
-        Saving user in the database after the subscription process.
+            Registering the new user in the firebase database
          */
-        /*TweetDatabaseDeux db = TweetDatabaseDeux.getAppDatabase(this);
 
-        UserEntity userEntity = new UserEntity(user.email,user.password,user.nickname);
 
-        db.UserDao().insert(userEntity);*/
+
+
     }
 
-    public void saveUserToSession(User user){
-
-        /*
-        Storing the user in the shared preferences.
-
-        As there is no way to store an object, we used Gson library to transform our object in a gson structured string that
-        will then be change into a user object again when we need to check stuff...
-         */
-        Gson gson = new Gson();
-        String userInfoString = gson.toJson(user);
-
-
-
-        SharedPreferences.Editor editor = getSharedPreferences(getResources().getString(R.string.sharedPref), MODE_PRIVATE).edit();
-        editor.putString("user",userInfoString);
-        editor.apply();
-        editor.commit();
-    }
 }
