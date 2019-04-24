8baf0c2fd342d5686a48b2cd9ef43547274f5985
loic <loic.python@gmail.com>
2019-04-24 17:01:20 +0200

    Ouiiiiiiiiiiiiiiiiiii

--- app/src/main/java/ch/ribeiropython/twitterproject/api/TweetHelper.java
+++ app/src/main/java/ch/ribeiropython/twitterproject/api/TweetHelper.java
@@ -1,5 +1,6 @@
 package ch.ribeiropython.twitterproject.api;
 
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
@@ -16,6 +17,14 @@ public class TweetHelper {
     public static Query getAllTweetbyDate(){
         return getAllTweet()
                 .orderBy("Date");
+    }
+
+    public static Task<Void> deleteTweet(String uid) {
+        return TweetHelper.getAllTweet().document(uid).delete();
+    }
+
+    public static Task<Void> updateTweet(String newTweetMsg, String newHashtags, String idTweet) {
+        return TweetHelper.getAllTweet().document(idTweet).update("Message",newTweetMsg,"Hastags",newHashtags);
     }
 
 }
